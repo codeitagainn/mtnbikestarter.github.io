ee3b433a82b2109b4c8f564764d453c467a96879
Jack Burum <jtburum@gmail.com>
2017-12-26 17:49:07 -0500

    update-bike-displays

--- js/script.js
+++ js/script.js
@@ -13,7 +13,29 @@ $(document).ready(function(){
   disableLowBudgetsForCcAndDh()
   underlineDarkGreenQuestionHeaders()
   minusPlusToggle()
+  initializeDropDown()
+  underlineActiveProject()
 })
+
+function initializeDropDown() {
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+    }
+  );
+}
+
+function underlineActiveProject() {
+  $('.active').mouseout(function() {
+    $(this).css('border-bottom', '2px solid #06D6A0')
+  })
+}
 
 function minusPlusToggle() {
   $('.terms').children().click(function() {
