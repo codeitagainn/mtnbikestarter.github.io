71f60ffd973d1a3a66332bd88d80c2a29f5bdec0
Jack Burum <jtburum@gmail.com>
2018-01-22 13:45:15 -0800

    speed of animation

--- js/script.js
+++ js/script.js
@@ -245,7 +245,7 @@ function envCheckForUrlPush(bike_file) {
 function slideOutRight() {
   $('.next-question').click(function() {
     let current_question = $('.displayed')
-    current_question.hide("slide", {direction: "right"}, 1800);
+    current_question.hide("slide", {direction: "right"}, 1200);
     getNextQuestion(current_question)
   })
 }
